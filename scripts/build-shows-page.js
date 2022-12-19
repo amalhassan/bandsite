9313d341f8d1686b10d302f4d34c8ed09d4e5d91
Amal Hassan <amalmhass@gmail.com>
2022-12-18 22:33:43 -0500

    added selected feature for rows in shows page and relative time display for comment dates in bio page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,21 +1,12 @@
 import  elementGenerator  from "./elementGenerator.js";
-const showsTitleSection = document.getElementById('title-container'); 
+// declared variables 
 const showsSection = document.getElementById('info-section');
 const titleDateLabel = elementGenerator('h4', {class:'shows__label-item shows__label-item--tablet-desktop'}, 'Date');
 const titleVenueLabel = elementGenerator('h4', {class:'shows__label-item shows__label-item--tablet-desktop'}, 'Venue');
 const titleLocationLabel = elementGenerator('h4', {class:'shows__label-item shows__label-item--tablet-desktop'}, 'Location');
 let showLabelContainer = elementGenerator('ul', {class: 'shows__labels shows__label-item--tablet-desktop'}, titleDateLabel, titleVenueLabel, titleLocationLabel);
 let showsContainer;
-
-window.addEventListener("load", () => {
-    // const titleDateLabel = elementGenerator('h4', {class:'shows__label-item shows__label-item--tablet-desktop'}, 'Date');
-    // const titleVenueLabel = elementGenerator('h4', {class:'shows__label-item shows__label-item--tablet-desktop'}, 'Venue');
-    // const titleLocationLabel = elementGenerator('h4', {class:'shows__label-item shows__label-item--tablet-desktop'}, 'Location');
-    // let showLabelContainer = elementGenerator('ul', {class: 'shows__labels'}, titleDateLabel, titleVenueLabel, titleLocationLabel);
-    // console.log(showLabelContainer);
-    // showsTitleSection.append(showLabelContainer);
-})
-
+// array to store show information
 let shows = [
     {
         date: "Mon Sept 06 2021",
@@ -49,35 +40,40 @@ let shows = [
     },
 ];
 console.log(shows);
-
+// function to display show information
 const displayShows = (showData) => {
     showData.map((show) => {
+        // created elements for date information and added styling classes
         let dateLabel = elementGenerator('p', {class: 'shows__label-item--mobile'}, 'Date');
         let showDate = elementGenerator('p', {class: 'shows__info shows__info--date'}, show.date);
         let dateWrapper = elementGenerator('div', {class: 'shows__field-wrapper'}, dateLabel, showDate);
-    
+        // created elements for venue information and added styling classes
         let venueLabel = elementGenerator('p', {class: 'shows__label-item--mobile'}, 'Venue');
         let showVenue = elementGenerator('p', {class: 'shows__info'}, show.venue);
         let venueWrapper = elementGenerator('p', {class: 'shows__field-wrapper'}, venueLabel, showVenue);
-    
+        // created elements for location information and added styling classes
         let locationLabel = elementGenerator('p', {class: 'shows__label-item--mobile'}, 'Location');
         let showLocation = elementGenerator('p',  {class: 'shows__info'}, show.location);
         let locationWrapper = elementGenerator('div', {class: 'shows__field-wrapper'}, locationLabel, showLocation);
+        // created element for button and added styling class
         let ticketBtn = elementGenerator('button', {class: 'shows__btn'}, 'Buy Tickets');
+        // created element dor parent container, added styling class, and appended children
         showsContainer = elementGenerator('div', {class: 'shows__info-container'}, dateWrapper, venueWrapper, locationWrapper, ticketBtn);
+        // appended showsContainer to showsSection
         showsSection.appendChild(showsContainer);
     })
     showsSection.prepend(showLabelContainer);
 };
-
 displayShows(shows);
-console.log(showsContainer);
+// get rows with show information
 const showRows = document.getElementsByClassName('shows__info-container');
-console.log(showRows);
-Array.from(showRows).forEach((element, index) => {
-    let current = document.querySelectorAll('selected');
-    console.log(current);
+// function to added .selected class to a row when selected and remove when next row is selected
+Array.from(showRows).forEach((element) => {
     element.addEventListener('click', () => {
+        let selected = document.querySelector('.selected');
+        if(selected) {
+            selected.classList.remove('selected');
+        }
         element.classList.toggle("selected");
     });
-});
+})
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,7 +1,41 @@
 import  elementGenerator  from "./elementGenerator.js";
-//validation of user's submission
+// declared variables 
+const form = document.getElementById('form');
+// console.log(form);
+let commentSection = document.getElementById('commentBox');
+let containerDiv;
+let avatarImg;
+let commentDiv;
+let commentElement;
+let nameDiv;
+let nameElement;
+let dateElement;
+// function returning a relative date format for comment dates 
+const relativeTimeFormat = new Intl.RelativeTimeFormat('en', {
+    numeric: 'auto',
+})
+const timePeriod = [
+    {period: 60, name: 'seconds'},
+    {period: 60, name: 'minutes'},
+    {period: 24, name: 'hours'},
+    {period: 7, name: 'days'},
+    {period: 4.34524, name: 'weeks'},
+    {period: 12, name: 'months'},
+    {period: Number.POSITIVE_INFINITY, name: 'years'}
+]
+const formatCommentDate = (date) => {
+    let difference = (date - new Date()) / 1000;
+    timePeriod.find((obj) => {
+        if (Math.abs(difference) < obj.period) {
+            date = relativeTimeFormat.format(Math.round(difference), obj.name);
+            return true
+        }
+        difference = difference / obj.period;
+    })
+    return date;
+}
+// validation of user's submission
 const userInput = document.querySelectorAll("input, textarea");
- 
 userInput.forEach(input => {
     input.addEventListener("invalid", (event) => {
             input.classList.add("conversation__input-error");
@@ -14,93 +48,63 @@ userInput.forEach(input => {
         } 
       });
 });
-
-// variables 
-const form = document.getElementById('form');
-console.log(form);
-let commentSection = document.getElementById('commentBox');
-let containerDiv;
-let avatarImg;
-let commentDiv;
-let commentElement;
-let nameDiv;
-let nameElement;
-let dateElement;
-
-// second argument for toLocaleDateString() method - formats date as MM/dd/YYYY
-const dateFormat = {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-}
 // array to store comments
 let commentArray = [
     {
         name: "Connor Walton", 
-        date: new Date('02/17/2021').toLocaleDateString('en-US', dateFormat), 
+        date: new Date('02/17/2021'), 
         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
     }, 
     {
         name: "Emilie Beach", 
-        date: new Date('01/09/2021').toLocaleDateString('en-US', dateFormat), 
+        date: new Date('01/09/2021'), 
         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
     }, 
     {
         name: "Miles Acosta", 
-        date: new Date('12/20/2020').toLocaleDateString('en-US', dateFormat), 
+        date: new Date('12/20/2020'), 
         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
     }
 ];
-console.log(commentArray);
-
-const defaultDisplayComment = (ca) => {
-    ca.forEach((obj) => {
-        // created nameElement and added styling. Assigned obj.name to innerHTML
-        nameElement = elementGenerator('h4', {class: 'conversation__name'});
-        nameElement.innerHTML = obj.name;
-
-        // created dateElement and added styling. Assigned obj.date to innerHTML
-        dateElement = elementGenerator('p', {class: 'conversation__date'});
-        dateElement.innerHTML = obj.date;
-
-        // created nameDiv, added styling and appended childrem: nameElement, dateElement
-        nameDiv = elementGenerator('div', {class: 'conversation__title-section'}, nameElement, dateElement);
-        
-        // created commentElement and added styling. Assigned obj.comment to innerHTML
-        commentElement = elementGenerator('p', {class: 'conversation__text'});
-        commentElement.innerHTML = obj.comment;
-
-        // created commentDiv, added styling and appended children: nameDiv, commentDiv
-        commentDiv = elementGenerator('div', {class: 'conversation__comment-div'}, nameDiv, commentElement);
-        
-        // created avatarImg and added styling
-        avatarImg = elementGenerator('img', {class: 'conversation__avatar', src: "../assets/images/Mohan-muruge.jpg"}); 
-
-        // created containerDiv, added styling and appended children: avatarImg, commentDiv
-        containerDiv = elementGenerator('div', {class: 'conversation__comment-wrapper'}, avatarImg, commentDiv);
-        
-        //appended containerDiv element into commentSection element
-        commentSection.append(containerDiv);
-    })  
+// function to create elements and display comments
+const displayComment = (ca) => {
+    // created nameElement and added styling. Assigned obj.name to innerHTML
+    nameElement = elementGenerator('h4', {class: 'conversation__name'});
+    nameElement.innerHTML = ca.name;
+    // created dateElement and added styling. Assigned obj.date to innerHTML
+    dateElement = elementGenerator('p', {class: 'conversation__date'});
+    dateElement.innerHTML = formatCommentDate(ca.date);
+    // created nameDiv, added styling and appended childrem: nameElement, dateElement
+    nameDiv = elementGenerator('div', {class: 'conversation__title-section'}, nameElement, dateElement);
+    // created commentElement and added styling. Assigned obj.comment to innerHTML
+    commentElement = elementGenerator('p', {class: 'conversation__text'});
+    commentElement.innerHTML = ca.comment;
+    // created commentDiv, added styling and appended children: nameDiv, commentDiv
+    commentDiv = elementGenerator('div', {class: 'conversation__comment-div'}, nameDiv, commentElement);
+    // created avatarImg and added styling
+    avatarImg = elementGenerator('img', {class: 'conversation__avatar', src: "../assets/images/Mohan-muruge.jpg"}); 
+    // created containerDiv, added styling and appended children: avatarImg, commentDiv
+    containerDiv = elementGenerator('div', {class: 'conversation__comment-wrapper'}, avatarImg, commentDiv);
+    //comment div prepended into commentSection
+    commentSection.prepend(containerDiv);
 }
-defaultDisplayComment(commentArray);
-
-let createComment = (e) => {
+commentArray.forEach((obj) => {
+    displayComment(obj);
+})
+// function to retrieve submission from user, store new comment into commentArray, and display new comment
+const createAndDisplayNewComment = (e) => {
     e.preventDefault();
-    userInput.forEach(input => {
-        input.classList.remove("conversation__input-error");
-    });
     let commentName = form[0].value;
-    console.log(commentName);
+    // console.log(commentName);
     let commentText = form[1].value
-    console.log(commentText);
+    // console.log(commentText);
     let commentDate = Date.now();
-    console.log(commentDate);
+    // console.log(commentDate);
     form.reset();
-    let addToCommentArray = (cn, fd, ct) => {
+    let addToCommentArray = (cn, cd, ct) => {
         let newComment = {
             name: cn,
-            date: fd,
+            date: cd,
             comment: ct
         }
         commentArray.push(newComment);
@@ -110,39 +114,8 @@ let createComment = (e) => {
         commentArray.sort(
             (a, b) => b.date - a.date,
         );
-        commentArray.forEach((item) => {
-            item.date = new Date(item.date).toLocaleDateString('en-US', dateFormat);
-        });
-        console.log(commentArray);
     }
     addToCommentArray(commentName, commentDate, commentText);
-    const displayComment = (ca) => {
-        // created nameElement and added styling. Assigned obj.name to innerHTML
-        nameElement = elementGenerator('h4', {class: 'conversation__name'});
-        nameElement.innerHTML = ca[0].name;
-
-        // created dateElement and added styling. Assigned obj.date to innerHTML
-        dateElement = elementGenerator('p', {class: 'conversation__date'});
-        dateElement.innerHTML = ca[0].date;
-
-        // created nameDiv, added styling and appended childrem: nameElement, dateElement
-        nameDiv = elementGenerator('div', {class: 'conversation__title-section'}, nameElement, dateElement);
-
-        // created commentElement and added styling. Assigned obj.comment to innerHTML
-        commentElement = elementGenerator('p', {class: 'conversation__text'});
-        commentElement.innerHTML = ca[0].comment;
-
-        // created commentDiv, added styling and appended children: nameDiv, commentDiv
-        commentDiv = elementGenerator('div', {class: 'conversation__comment-div'}, nameDiv, commentElement);
-
-        // created avatarImg and added styling
-        avatarImg = elementGenerator('img', {class: 'conversation__avatar', src: "../assets/images/Mohan-muruge.jpg"}); 
-
-        // created containerDiv, added styling and appended children: avatarImg, commentDiv
-        containerDiv = elementGenerator('div', {class: 'conversation__comment-wrapper'}, avatarImg, commentDiv);
-        //comment div prepended into commentSection
-        commentSection.prepend(containerDiv);
-    }
-    displayComment(commentArray);
+    displayComment(commentArray[0]);
 }
-form.addEventListener('submit', createComment);
+form.addEventListener('submit', createAndDisplayNewComment);
